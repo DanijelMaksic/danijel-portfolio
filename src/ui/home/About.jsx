f2b856e8f4fd27d0a2bac3b1428e074ab402ad84
DanijelMaksic <danijel.maksic77@gmail.com>
2025-07-25 13:25:53 +0200

    Adjusted text opacity

--- src/ui/home/About.jsx
+++ src/ui/home/About.jsx
@@ -30,7 +30,7 @@ function About() {
                   {t('About.name')}
                </span>
 
-               <div className="flex flex-col gap-3 text-primary-600 leading-8 md:leading-9 xs:leading-10 2xs:leading-11">
+               <div className="flex flex-col gap-3 text-primary-700 leading-8 md:leading-9 xs:leading-10 2xs:leading-11">
                   <span>{t('About.p1')}</span>
 
                   <span>
