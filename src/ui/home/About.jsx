4d90c74579005d6379e6dc5f0bd3a2993a7146dc
DanijelMaksic <danijel.maksic77@gmail.com>
2025-07-29 16:30:10 +0200

    Updated about section image

--- src/ui/home/About.jsx
+++ src/ui/home/About.jsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { motion } from 'motion/react';
-import AboutImage from './../../../assets/about-image.webp';
+import AboutImage from './../../../assets/about-image.jpg';
 
 function About() {
    const { t } = useTranslation();
