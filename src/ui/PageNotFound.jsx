fdb1b86c12157e11ffb79a0cd911d5b9309543f7
DanijelMaksic <danijel.maksic77@gmail.com>
2025-08-06 14:47:38 +0200

    Added translation for PageNotFound page

--- src/ui/PageNotFound.jsx
+++ src/ui/PageNotFound.jsx
@@ -1,15 +1,18 @@
+import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
 function PageNotFound() {
+   const { t } = useTranslation();
+
    return (
       <div className="bg-primary-50 dark:bg-primary-100 h-screen flex items-center justify-center text-primary-900">
          <div className="flex flex-col items-center gap-6 md:gap-4 xs:gap-6 bg-primary-100 dark:bg-primary-200/45 px-12 sm:px-4 sm:mx-12 py-10 shadow--sm dark:shadow-none rounded-xl mb-32 dark:border-2 dark:border-primary-200">
             <p className="text-5xl text-center font-semibold">
-               Page not Found...
+               {t('PageNotFound')}
             </p>
             <Link to="/">
                <button className="text-xl xs:text-2xl underlined-text text-primary-600">
-                  &larr; &nbsp;Go back
+                  &larr; &nbsp;{t('GoBack')}
                </button>
             </Link>
          </div>
